ada2eda289888bf3bda8439a58375f22b5fb9c89
Daniel Liebner <dliebner@gmail.com>
2025-11-11 17:53:16 -0500

    Update install script: install chromium dependencies (for puppeteer)

--- install/install.sh
+++ install/install.sh
@@ -194,6 +194,9 @@ su dtcdn -c "composer require dliebner/backblaze-b2:dev-master"
 su - dtcdn -c "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash"
 su - dtcdn -c "nvm install --lts"
 
+# install chromium dependencies (for puppeteer)
+sudo apt install libasound2 libatk-bridge2.0-0 libatk1.0-0 libatspi2.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libdrm2 libexpat1 libgbm1 libglib2.0-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libudev1 libuuid1 libx11-6 libx11-xcb1 libxcb-dri3-0 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxkbcommon0 libxrandr2 libxrender1 libxshmfence1 libxss1 libxtst6
+
 # go back to install dir
 cd /home/dtcdn/install
 
